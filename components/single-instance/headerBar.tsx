dff801df2760aa2c502735d6aa06e891d6e5b94c
buchiya4th <buchiya4th@gmail.com>
2020-08-21 06:07:29 +0900

    React hookを使ったヘッダーナビアイコンのクリック時のclassのトグル機能の実装

--- components/single-instance/headerBar.tsx
+++ components/single-instance/headerBar.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideNav from './sideNav'
 import styles from '../../styles/components/single-instance/headerBar.module.scss'
-// import classnames from 'classnames'
+import classnames from 'classnames'
 
 const HeaderBar: React.FC = () => {
+  const [isActive, setActive] = useState(false)
+
   return (
     <div className={styles.headerBar}>
-      <div className={styles.navMenu}>
+      <div className={classnames(styles.navMenu, { 'is-active': isActive })} onClick={() => setActive(!isActive)}>
         <span className={styles.navMenu_line}></span>
         <span className={styles.navMenu_line}></span>
         <span className={styles.navMenu_line}></span>
