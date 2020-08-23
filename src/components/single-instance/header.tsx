e0922e7626a7d6852b9b5b982f4dbba34052ab2c
buchiya4th <buchiya4th@gmail.com>
2020-08-24 05:50:17 +0900

    add header title

--- src/components/single-instance/header.tsx
+++ src/components/single-instance/header.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeaderBar from '@/src/components/single-instance/headerBar'
+import HeaderTitle from '@/src/components/single-instance/headerTitle'
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <HeaderBar />
+      <HeaderTitle />
     </header>
   )
 }
---
+++ src/components/single-instance/headerTitle.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '@/styles/components/single-instance/headerTitle.module.scss'
+
+const HeaderTitle: React.FC = () => {
+  return (
+    <div className={styles.headerTitle}>
+      <p className={styles.siteLog}>
+        <img src="/img/logo.svg" alt="ぶちろぐ" />
+      </p>
+      <div className={styles.siteLead}>
+        このサイトは、Webと音楽を愛する人の提供でお送りします。
+      </div>
+    </div>
+  )
+}
+
+export default HeaderTitle
