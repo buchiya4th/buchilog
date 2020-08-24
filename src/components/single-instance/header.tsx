fa282607751be5902e31f5950de2acf7090ed169
buchiya4th <buchiya4th@gmail.com>
2020-08-25 06:44:40 +0900

    add headerFollow component

--- src/components/single-instance/header.tsx
+++ src/components/single-instance/header.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderBar from '@/src/components/single-instance/headerBar'
 import HeaderTitle from '@/src/components/single-instance/headerTitle'
+import HeaderFollow from '@/src/components/single-instance/headerFollow'
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <HeaderBar />
       <HeaderTitle />
+      <HeaderFollow />
     </header>
   )
 }
