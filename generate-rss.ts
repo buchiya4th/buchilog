e4d33550b595596e478f278b68c9658142e77dde
buchiya4th <buchiya4th@gmail.com>
2021-04-30 05:57:20 +0900

    fix: feed logic.

--- generate-rss.ts
+++ generate-rss.ts
@@ -1,23 +1,30 @@
-const fs = require('fs')
-const path = require('path')
-const matter = require('gray-matter')
+import * as fs from "fs"
 const fsPromise = fs.promises
+import { join } from "path"
+import matter from "gray-matter"
+
+type Post = {
+  slug: string
+  title: string
+  description: string
+  date: number
+}
 
 const metaData = {
   title: 'ぶちろぐ',
   description: 'このサイトは、Webと音楽を愛する人の提供でお送りします。',
 }
 const POST_URL = `${process.env.DOMAIN}/posts`
-const RSS_URL = `${process.env.DOMAIN}/rss.xml`
-const postDirPath = path.join(process.cwd(), "posts")
-const outputPostDirPath = path.join(process.cwd(), "out/posts")
+const RSS_URL = `${process.env.DOMAIN}/feed.xml`
+const postDirPath = join(process.cwd(), "_posts")
+const outputPostDirPath = join(process.cwd(), "out/posts")
 
-const getPost = async (slug) => {
-  const fullPath = path.join(postDirPath, `${slug}.md`)
+export const getPost = async (slug: string) => {
+  const fullPath = join(postDirPath, `${slug}.md`)
   const fileContents = await fsPromise.readFile(fullPath, "utf8")
   const { data } = matter(fileContents)
 
-  const post = {
+  const post: Post = {
     slug: slug,
     title: data["title"],
     description: data["description"],
@@ -37,14 +44,15 @@ const getAllPosts = async () => {
   return posts
 }
 
-const createFeed = (post) => `    <item>
+const createFeed = (post: Post) => `    <item>
       <title>${post.title}</title>
       <link>${POST_URL}/${post.slug}</link>
+      <guid>${POST_URL}/${post.slug}</guid>
       <pubDate>${new Date(post.date).toUTCString()}</pubDate>
       <description>${post.description}</description>
     </item>`
 
-const writeRss = async (filePath, content) => {
+const writeRss = async (filePath: string, content: string) => {
   try {
     await fsPromise.writeFile(filePath, content, "utf-8")
   } catch (err) {
@@ -57,32 +65,20 @@ const generateRss = async () => {
   const lastBuildDate = new Date(posts.slice(-1)[0].date).toUTCString()
   const feeds = posts.map((post) => createFeed(post))
 
-  const rss = `<?xml version="1.0" encoding="UTF-8"?>
-<rss version="2.0"
-  xmlns:content="http://purl.org/rss/1.0/modules/content/"
-  xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-  xmlns:dc="http://purl.org/dc/elements/1.1/"
-  xmlns:atom="http://www.w3.org/2005/Atom"
-  xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
-  xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
-  xmlns:georss="http://www.georss.org/georss"
-  xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"
->
+  const rss = `<?xml version="1.0" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>${metaData.title}</title>
-    <atom:link href="${RSS_URL}" rel="self" type="application/rss+xml" />
     <link>${process.env.DOMAIN}</link>
     <description>${metaData.description}</description>
-    <lastBuildDate>${lastBuildDate}</lastBuildDate>
     <language>ja</language>
-    <sy:updatePeriod>hourly</sy:updatePeriod>
-    <sy:updateFrequency>1</sy:updateFrequency>
-    <generator>https://wordpress.org/?v=5.1.1</generator>
+    <lastBuildDate>${lastBuildDate}</lastBuildDate>
+    <atom:link href="${RSS_URL}" rel="self" type="application/rss+xml" />
 ${feeds.join("\n")}
   </channel>
 </rss>`
 
-  const filePath = path.join(process.cwd(), "out/rss.xml")
+  const filePath = join(process.cwd(), "out/feed.xml")
 
   await writeRss(filePath, rss)
 }
